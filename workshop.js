91f4df9dd24bfbb1521917192cb85b9acf68a579
AlastairGMc <72364426+AlastairGMc@users.noreply.github.com>
2020-10-25 16:37:18 -0700

    Create workshop.js

---
+++ workshop.js
@@ -0,0 +1,26 @@
+<script type='text/javascript'>
+    //<![CDATA[
+
+
+    //const downArrow = '&#9660; ';
+    //const rightArrow = '&#9654; ';
+
+    const downArrow = '▼';
+    const rightArrow = '▶';
+
+    const upArrow = '&#9650; ';
+    const plus = '&#65291;';
+    const minus = '&#65293;';
+    const downChevron = '&#8964;';
+    const upChevron = '&#8963;';
+
+    const upTriangle = '⏶';
+    const downTriangle = '⏷';
+
+    let titleBtnIcon = [rightArrow, downArrow];			// idx 0 means the container is hidden; idx 1 means the container is visible
+    let descripBtnIcon = [downTriangle, upTriangle];
+	let scenes = [];
+	let chapters = [];
+	let sections = [];
+  
+</script>
