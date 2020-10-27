268b21eb5cc1eacc9b42a26648b3896b95f69e6b
AlastairGMc <72364426+AlastairGMc@users.noreply.github.com>
2020-10-27 11:34:44 -0700

    Update workshop.js

--- workshop.js
+++ workshop.js
@@ -1,9 +1,9 @@
       function gotoPage(url){
-	alert(url);
+	//alert(url);
       	window.location.href = url;
       }
 
       function clk(divID){
 	var div = document.getElementById(divID);
-	div.style.display = (div.style.display == &#39;none&#39; ? &#39;block&#39; : &#39;none&#39;);
+	div.style.display = (div.style.display == 'none' ? 'block' : 'none');
       }
